fe2f48877870ef4052868d55270055a71a789a23
LeinHaruki <rowenromert@gmail.com>
2025-10-26 16:25:07 +0800

    Added comment by Lein in lein-branch

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
 
+// Added by Lein for Git Branch test
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
@@ -18,3 +21,5 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
+
+
